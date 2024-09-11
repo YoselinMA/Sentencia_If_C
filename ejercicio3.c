a9530d24e1be8e70a9ca7c722e3217d862e07f67
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-09-10 22:34:44 -0600

    Create ejercicio3.c

---
+++ ejercicio3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    int x;
+    puts("Ingresa tu calificación"); //imprime los mensajes dando salto de linea sin colocar un slash
+    scanf("%d", &x); //sirve para recivir datos del teclado
+
+    if(x >=70){
+    printf("Felicidades aprobaste\n");
+    }
+
+    printf("Ingresa una nueva calificación");
+
+
+}
