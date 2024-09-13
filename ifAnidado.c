285f7f8100a74a9690779f2890e0fdeca0c63915
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-09-12 21:18:18 -0600

    Create ifAnidado.c

---
+++ ifAnidado.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int numero = 135; 
+    if(numero < 100){
+        printf("El numero es menor que 100 \n");
+        if (numero > 50){
+            printf(" y mayor que 50 \n");
+        }
+    }
+    printf("Fin del programa \n");
+    system("Pause");
+    return 0;
+
+}
